9512a5d9724651485c17439deccd746b0e867b39
Leonardo Lourenco <lnddru@gmail.com>
2022-01-07 19:31:16 -0300

    Add files via upload

---
+++ Aula-07.py
@@ -0,0 +1,40 @@
+
+# Aula 07 : Funções Anonymous / Lambda em Python
+
+#double = lambda x: x * 2
+
+#print(double(5))
+
+
+"""
+
+
+# FILTER()
+
+lista = [1, 5, 4, 6, 8, 11, 3, 12]
+
+novaLista = list(filter(lambda x: (x%2 == 0), lista))
+
+print(novaLista)
+
+
+"""
+
+
+
+
+# Function MAP()
+
+lista = [1, 5, 4, 6, 8, 11, 3, 12]
+
+novaLista = list(map(lambda x: x * 2, lista))
+
+print(novaLista)
+
+
+
+
+
+
+
+

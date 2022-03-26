376655c6752422b29972b9b3f5f2d86b622a54c8
Leonardo Lourenco <lnddru@gmail.com>
2022-03-26 18:57:10 -0300

    Add files via upload

---
+++ Aula-12.py
@@ -0,0 +1,89 @@
+
+
+
+# Aula 12  -- Python Tuples  // Tuplas  em Python
+
+
+"""
+
+
+minhaTupla = ()
+print(minhaTupla)
+
+minhaTupla = (1, 2, 3)
+print(minhaTupla)
+
+minhaTupla = (1, "ola", 3.4)
+print(minhaTupla)
+
+minhaTupla = ("ola", [8, 4, 2], (1, 2, 3))
+print(minhaTupla)
+
+
+
+
+minhaTupla = 3, 4.6, "cat"
+print(minhaTupla)
+
+
+a, b, c  = minhaTupla
+print(a)
+print(b)
+print(c)
+
+
+
+
+minhaTupla  = ("oi")
+print(type(minhaTupla))
+
+minhaTupla  = ("oi", )
+print(type(minhaTupla))
+
+
+minhaTupla  = "oi",
+print(type(minhaTupla))
+
+
+
+"""
+
+
+# Acessando os valores de uma TUPLA
+"""
+
+minhaTupla = ('c', 'o', 'f', 'f', 'e', 'e')
+print(minhaTupla[0])
+print(minhaTupla[5])
+print(minhaTupla[2])
+
+
+
+
+# Tuplas Aninhadas
+
+minhaTUPLA = ("mouse", [8,4,6], (1,2,3))
+
+print(minhaTUPLA[0][3])
+
+
+print(minhaTUPLA[1][1])
+
+
+
+"""
+
+
+
+minhaTupla = ('c', 'o', 'f', 'f', 'e', 'e')
+print(minhaTupla[-1])
+
+print(minhaTupla[-6])
+
+
+
+
+
+
+
+
